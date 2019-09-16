e10d6c71996cad671c16cfb1f873d3eea5aaf0a1
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-16 16:49:43 -0500

    finished types and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,40 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
-        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 24;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Lemon is cool!";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+        int x = 4;
+        x += 5;
+
+        int x = 3;
+        int y = 4;
+         y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
     }
 }
