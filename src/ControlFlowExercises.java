8a3aaa886dc1af61db4c1a5f8aa9b3b7ceae8437
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-17 15:27:29 -0500

    loop basics refactored while and do-while with for loop exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -8,6 +8,10 @@ public class ControlFlowExercises {
 //        while(i <= 15) {
 //            System.out.println(i++);
 //        }
+        // Refactor of Above Code
+//        for(int i = 5; i <= 15;i++) {
+//            System.out.println(i);
+//        }
 
 
 //        int i = 100;
@@ -15,14 +19,21 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i -= 5;
 //        }while(i >= -10);
+        // Refactor of Above Code
+//        for(int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+
 
 //        int i = 2;
 //        do {
 //            System.out.println(i);
 //            i *= Math.pow(i,1);
 //        } while (i <= 1000000);
-
-
+        // Refactor of Above Code
+//        for(int i = 2; i <= 1000000;i *= Math.pow(i,1)) {
+//            System.out.println(i);
+//        }
 
     }
 }
