5988bd6c9221094594407355f2f84bb57203144d
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-17 15:22:22 -0500

    loop basics do-while exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,28 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-    int i = 5;
-    while(i <= 15) {
-        System.out.println(i++);
-    }
+
+
+
+//        int i = 5;
+//        while(i <= 15) {
+//            System.out.println(i++);
+//        }
+
+
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        }while(i >= -10);
+
+//        int i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= Math.pow(i,1);
+//        } while (i <= 1000000);
+
+
 
     }
 }
