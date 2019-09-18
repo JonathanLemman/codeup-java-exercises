91e619b41572087147c36c92359fde6301d128db
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-18 12:20:54 -0500

    string exercise basic

---
+++ src/PracticeFun.java
@@ -0,0 +1,6 @@
+public class PracticeFun {
+    public static void main(String[] args) {
+        // convert word to pig latin word (assume word doesn't begin with a vowel or end in ay)
+        //        String cat = "cat"; // "atcay"
+    }
+}
---
+++ src/StringExercise.java
@@ -0,0 +1,24 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+//        String message = "We don't need no education";
+//        System.out.println(message + "\n" + message.replace("education", "thought control"));
+
+//        String message = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(message);
+
+//        String message = "In windows, the main drive is usually C:\\";
+//        System.out.println(message);
+
+//        String message = "I can do backslashes \\, double backslashes \\\\,\n" +
+//                "and the amazing triple backslash \\\\\\!";
+//        System.out.println(message);
+
+
+
+
+
+
+
+    }
+}
