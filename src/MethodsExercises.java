57a75d784133c76f62b2eabf4c6889315a3e1e52
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-19 10:48:34 -0500

    basic arithmetic first exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,34 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+    // *************** Main **************************
+
+        System.out.println(returnAddition(3,5));
+        System.out.println(returnSubtraction(5,2));
+        System.out.println(returnMultiplication(3,5));
+        System.out.println(returnDivision(10,3));
+
+    }
+
+    // **************** Methods ***********************
+    public static int returnAddition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int returnSubtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int returnMultiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static int returnDivision(int num1, int num2) {
+        return num1 / num2;
+    }
+
+
+
+
+
+
+}
