98dac25f0cdc4ca877bc1e638d877af50b2b7abd
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-19 11:50:54 -0500

    finished the recursion for multiply method

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -5,7 +5,7 @@ public class MethodsExercises {
         System.out.println(returnAddition(3,5));
         System.out.println(returnSubtraction(5,2));
         System.out.println(returnMultiplication(3,5));
-        System.out.println(returnDivision(10,0));
+        System.out.println(returnDivision(10,2));
         System.out.println(returnModulus(10,3));
 
     }
@@ -19,8 +19,24 @@ public class MethodsExercises {
         return num1 - num2;
     }
 
+    // Basic Multi with *
+//    public static int returnMultiplication(int num1, int num2) {
+//        return num1 * num2;
+//    }
+    // Multi with Loop
+//    public static int returnMultiplication(int num1, int num2) {
+//        int total = 0;
+//        for(int i = 0; i < num1; i++) {
+//            total += num2;
+//        }
+//        return total;
+//    }
+    // Multi with Recursion
     public static int returnMultiplication(int num1, int num2) {
-        return num1 * num2;
+        if((num1 == 0) || (num2 == 0)) {
+            return 0;
+        }
+        return (num1 + returnMultiplication(num1, num2 - 1));
     }
 
     public static int returnDivision(int num1, int num2) {
