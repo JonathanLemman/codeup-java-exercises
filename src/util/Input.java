a3b5574e5fab0edb10867673605e433b12395047
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-24 14:52:28 -0500

    finished exercise 3 before bonus on oop

--- src/util/Input.java
+++ src/util/Input.java
@@ -43,20 +43,11 @@ public class Input {
     }
 
     public double getDouble(){
-        return 0;
+        return scanner.nextDouble();
     }
 
 
-    public static void main(String[] args){
-        Input userInput = new Input();
-        System.out.println(userInput.getString());
-        System.out.println("type yes or no");
-        System.out.println(userInput.yesNo());
-        System.out.println(userInput.getInt(1, 10));
-        System.out.println(userInput.getInt());
-        System.out.println(userInput.getDouble(1, 10));
 
-    }
 
 
 }
