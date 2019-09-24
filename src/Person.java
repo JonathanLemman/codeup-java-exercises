884896086562ea7b734c9c504659b7fe9ed21130
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-24 12:27:16 -0500

    person exercise

---
+++ src/Person.java
@@ -0,0 +1,55 @@
+
+public class Person {
+
+    private String name;
+
+    //************* Methods *************
+    // returns the person's name
+    public String getName(){
+        return this.name;
+    }
+    // changes the name property to the passed value
+    public void setName(String name){
+        this.name = name;
+    }
+    // prints a message to the console using the person's name
+    public void sayHello(){
+        System.out.format("Hello, %s!", name);
+    }
+
+    //************** Constructor *********
+    public Person(String name) {
+        this.name = name;
+    }
+
+
+    public static void main(String[] args){
+
+//        Person lemon = new Person("Lemon");
+//        System.out.println(lemon.getName());
+//        lemon.setName("Jonathan");
+//        lemon.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName())); // True *** True
+//        System.out.println(person1 == person2);// True *** False
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
+
+
+    }
+
+
+}
