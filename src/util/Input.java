8abcbe3e0d904ccebd9da70f546477251cfaa81a
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-25 11:01:22 -0500

    finished refactoring Input class

--- src/util/Input.java
+++ src/util/Input.java
@@ -5,45 +5,55 @@ public class Input {
     private Scanner scanner;
 
 
+
     // Constructor
     public Input() {
         this.scanner = new Scanner(System.in);
     }
 
+
+
+
     //Methods
     public String getString(){
-        return scanner.nextLine();
+        return this.scanner.nextLine();
     }
 
     public boolean yesNo(){
-        String check = scanner.nextLine();
+        String check = this.scanner.nextLine();
         return check.equalsIgnoreCase("yes") || check.equalsIgnoreCase("y");
     }
 
     public int getInt(int min, int max){
-        int userInput = scanner.nextInt();
+        System.out.println("Give me a number: ");
+        int userInput = Integer.parseInt(this.scanner.nextLine());
         if(userInput >= min && userInput <= max){
             return userInput;
         }else {
+            System.out.println("Out of range");
             return getInt(min, max);
         }
     }
 
     public int getInt(){
-        return scanner.nextInt();
+        System.out.println("Give me a number: ");
+        return Integer.parseInt(this.scanner.nextLine());
     }
 
     public double getDouble(double min, double max){
-        int userInput = scanner.nextInt();
+        System.out.println("Give me a decimal: ");
+        double userInput = Double.parseDouble(this.scanner.nextLine());
         if(userInput >= min && userInput <= max){
             return userInput;
         }else {
+            System.out.println("Out of range");
             return getDouble(min, max);
         }
     }
 
     public double getDouble(){
-        return scanner.nextDouble();
+        System.out.println("Give me a decimal: ");
+        return Double.parseDouble(this.scanner.nextLine());
     }
 
 
