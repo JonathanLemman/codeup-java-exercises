e74eb38320da29dcac155c69e7249a52b9de7b41
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-27 11:35:21 -0500

    refactored the exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,21 +1,21 @@
 package shapes;
 
 public class Rectangle {
-    protected int length;
-    protected int width;
+    protected double length;
+    protected double width;
 
     // constructor
-    public Rectangle(int length, int width) {
+    public Rectangle(double length, double width) {
         this.length = length;
         this.width = width;
     }
 
     // methods
-    public int getArea() {
+    public double getArea() {
         return this.length * this.width;
     }
 
-    public int getPerimeter() {
-        return 2 * this.length + 2 * this.width;
+    public double getPerimeter() {
+        return (2 * this.length) + (2 * this.width);
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,21 +2,21 @@ package shapes;
 
 public class Square extends Rectangle {
 
-    private int side;
 
     // constructor
-    public Square(int side) {
+    public Square(double side) {
         super(side, side);
-        this.side = side;
     }
 
     // methods
-    public int getArea() {
-        return (int) Math.pow(side, 2);
+    @Override
+    public double getArea() {
+        return Math.pow(super.length, 2);
     }
 
-    public int getPerimeter() {
-        return 4 * side;
+    @Override
+    public double getPerimeter() {
+        return 4 * super.length;
     }
 
 }
