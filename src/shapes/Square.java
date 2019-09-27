a7b69438cdcd91fc688d3d88b4882ab2d9a2b1ac
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-27 10:08:08 -0500

    finished poly exercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,10 +1,22 @@
 package shapes;
 
 public class Square extends Rectangle {
+
+    private int side;
+
     // constructor
-    public Square(int length, int width,int side) {
-        super(length, width);
-        super.length = side;
-        super.width = side;
+    public Square(int side) {
+        super(side, side);
+        this.side = side;
     }
+
+    // methods
+    public int getArea() {
+        return (int) Math.pow(side, 2);
+    }
+
+    public int getPerimeter() {
+        return 4 * side;
+    }
+
 }
