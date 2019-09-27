84d85637680fc659c1734ebad59ba74015d3837c
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-27 15:53:12 -0500

    played around with shapesTest

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,14 +4,10 @@ public class ShapesTest {
 
     public static void main(String[] args) {
         Measurable myShape = new Rectangle(5,4);
-        Measurable myShape1 = new Square(5);
+//        Measurable myShape = new Square(5);
 
         System.out.println(myShape.getPerimeter());
         System.out.println(myShape.getArea());
-
-        System.out.println(myShape1.getPerimeter());
-        System.out.println(myShape1.getArea());
-
 
 
 //        Rectangle box2 = new Square(5);
